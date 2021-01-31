2eed11934537f72a10ed710b2f0af6ecd506b8c0
unknown <newjincar33@gmail.com>
2021-01-31 22:25:07 +0900

    인젝터 코드 정리

--- Injector/main.cpp
+++ Injector/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <filesystem>
 #include <string>
 #include <Windows.h>
 #include <BlackBone/Process/Process.h>
 using namespace std;
 using namespace blackbone;
 
-wstring ExePath() {
-    WCHAR buffer[1024];
-    GetModuleFileName(NULL, buffer, 1024);
-    std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
-    return std::wstring(buffer).substr(0, pos);
-}
-
 int main(int argc, char* argv[]) {
 	Process process;
 	process.Attach(L"KakaoTalk.exe");
-	process.mmap().MapImage(ExePath() + L"\\KakaoTalk Image Downloader.dll");
+	auto result = process.mmap().MapImage(
+		filesystem::current_path().wstring() + L"\\KakaoTalk Image Downloader.dll"
+	);
+	if (!result) {
+		cout << hex << uppercase;
+		cout << "Failed to inject image (0x" << result.status << ")";
+	}
 	process.Detach();
+
+	return 0;
 }
